c28025621800b9cf40e92ff34f3d3e3da7a0f1f7
Caio César Gallo <caio.gallo@gmail.com>
2017-11-22 00:08:25 -0200

    commit inicial

---
+++ src/main/java/com/caiogallo/azul/marswalker/MarsWalkerApplication.java
@@ -0,0 +1,12 @@
+package com.caiogallo.azul.marswalker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarsWalkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarsWalkerApplication.class, args);
+	}
+}
