73dc20b64327b45498540bbd11176e1a4b03ea9d
Caio César Gallo <caio.gallo@gmail.com>
2017-11-22 00:31:45 -0200

    teste da string de resultado

--- src/test/java/com/caiogallo/azul/marswalker/engine/RobotTest.java
+++ src/test/java/com/caiogallo/azul/marswalker/engine/RobotTest.java
@@ -54,4 +54,59 @@ public class RobotTest {
 
     }
 
+    @Test
+    public void turnRightThreeTimes(){
+        robot.turn(Robot.Turn.RIGHT);
+        robot.turn(Robot.Turn.RIGHT);
+        robot.turn(Robot.Turn.RIGHT);
+
+        Assert.assertEquals(robot.getDirection(), Robot.Direction.WEST);
+        Assert.assertEquals(robot.getX(), 0);
+        Assert.assertEquals(robot.getY(), 0);
+
+    }
+
+    @Test
+    public void turnLeftFiveTimes(){
+        robot.turn(Robot.Turn.LEFT);
+        robot.turn(Robot.Turn.LEFT);
+        robot.turn(Robot.Turn.LEFT);
+        robot.turn(Robot.Turn.LEFT);
+        robot.turn(Robot.Turn.LEFT);
+
+        Assert.assertEquals(robot.getDirection(), Robot.Direction.WEST);
+        Assert.assertEquals(robot.getX(), 0);
+        Assert.assertEquals(robot.getY(), 0);
+
+    }
+
+    @Test
+    public void moveTwiceTurnRightMoveTwiceTurnRightMoveTwice(){
+        robot.move();
+        robot.move();
+        robot.turn(Robot.Turn.RIGHT);
+        robot.move();
+        robot.move();
+        robot.turn(Robot.Turn.RIGHT);
+        robot.move();
+        robot.move();
+
+        Assert.assertEquals(robot.getDirection(), Robot.Direction.SOUTH);
+        Assert.assertEquals(robot.getX(), 2);
+        Assert.assertEquals(robot.getY(), 0);
+        Assert.assertEquals("(2,0,S)", robot.toString());
+    }
+
+    @Test
+    public void moveTwiceAndTurnLeft(){
+        robot.move();
+        robot.move();
+        robot.turn(Robot.Turn.LEFT);
+
+        Assert.assertEquals(robot.getDirection(), Robot.Direction.WEST);
+        Assert.assertEquals(robot.getX(), 0);
+        Assert.assertEquals(robot.getY(), 2);
+        Assert.assertEquals("(0,2,W)", robot.toString());
+    }
+
 }
