32b516bc898184fc60c991a141bae136779327a2
Caio César Gallo <caio.gallo@gmail.com>
2017-11-23 23:20:04 -0200

    remove test nao utilizado

--- src/test/java/com/caiogallo/azul/marswalker/MarsWalkerApplicationTests.java
+++ src/test/java/com/caiogallo/azul/marswalker/MarsWalkerApplicationTests.java
@@ -1,16 +1,15 @@
 package com.caiogallo.azul.marswalker;
 
+import org.apache.naming.factory.BeanFactory;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MarsWalkerApplicationTests {
-
-	@Test
-	public void contextLoads() {
-	}
 
 }
